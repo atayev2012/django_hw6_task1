1d406ad59229e16fbc32ca38367149f2ed761963
Batyr Atayev <atayev2012@gmail.com>
2023-08-07 17:13:48 +0300

    Completed homwework

---
+++ logistic/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = ["id", "title", "description"]
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ["product", "quantity", "price"]
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ["id", "address", "positions"]
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for item in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=item["product"],
+                quantity=item["quantity"],
+                price=item["price"]
+            )
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        # здесь вам надо обновить связанные таблицы
+        stock_product_objects = StockProduct.objects.filter(stock=instance)
+        for item in positions:
+            stock_product = stock_product_objects.filter(product=item.get("product"))
+            stock_product.update(price=item.get("price"), quantity=item.get("quantity"))
+
+        return stock
